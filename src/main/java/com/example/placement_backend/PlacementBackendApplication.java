8d37d8b94a3ced30b704603c04cdff578c638000
Soumitra jena <131749408+somjena@users.noreply.github.com>
2023-11-17 21:26:10 +0530

    Created The Project and Except Security i have Download All The Dependency

---
+++ src/main/java/com/example/placement_backend/PlacementBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.placement_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlacementBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlacementBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/placement_backend/PlacementBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.placement_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlacementBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
