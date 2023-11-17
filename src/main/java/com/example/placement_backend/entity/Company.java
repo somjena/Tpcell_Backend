6301db2fb8eede49fc5caa52bdd0b585e7e15919
Soumitra jena <131749408+somjena@users.noreply.github.com>
2023-11-17 22:40:45 +0530

    Added The fields of Company Class
    Done And Let me Define  The Private  Fields Of The All The Classes

--- src/main/java/com/example/placement_backend/entity/Company.java
+++ src/main/java/com/example/placement_backend/entity/Company.java
@@ -1,4 +1,96 @@
 package com.example.placement_backend.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.time.LocalDateTime;
+
+@Entity
 public class Company {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String cname;
+    private  String address;
+    private String website;
+    private String email;
+    private String phone;
+    private LocalDateTime createdon;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCname() {
+        return cname;
+    }
+
+    public void setCname(String cname) {
+        this.cname = cname;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getWebsite() {
+        return website;
+    }
+
+    public void setWebsite(String website) {
+        this.website = website;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public LocalDateTime getCreatedon() {
+        return createdon;
+    }
+
+    public void setCreatedon(LocalDateTime createdon) {
+        this.createdon = createdon;
+    }
+
+    @Override
+    public String toString() {
+        return "Company{" +
+                "id=" + id +
+                ", cname='" + cname + '\'' +
+                ", address='" + address + '\'' +
+                ", website='" + website + '\'' +
+                ", email='" + email + '\'' +
+                ", phone='" + phone + '\'' +
+                ", createdon=" + createdon +
+                '}';
+    }
+
+    public Company(LocalDateTime createdon) {
+        this.createdon = createdon;
+    }
 }
+
+
