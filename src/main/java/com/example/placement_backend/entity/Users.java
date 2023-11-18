c9d6ffc5c85cbc1abeaa0c14f67b72fc13539da6
Saktimayee Nayak <131857334+Sakti2345@users.noreply.github.com>
2023-11-18 10:16:44 +0530

    done all the filds of users and the getter shetter methods also done
    commit

--- src/main/java/com/example/placement_backend/entity/Users.java
+++ src/main/java/com/example/placement_backend/entity/Users.java
@@ -1,4 +1,32 @@
 package com.example.placement_backend.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+import java.time.LocalDateTime;
+
+@Entity
 public class Users {
+    @Id
+    private String userid;
+    private String uname;
+    private String pwd;
+    private int cid;
+    private String role;
+    private boolean active;
+    private LocalDateTime createdon;
+
+    public Users(String userid, String uname, String pwd, int cid, String role, boolean active, LocalDateTime createdon) {
+        this.userid = userid;
+        this.uname = uname;
+        this.pwd = pwd;
+        this.cid = cid;
+        this.role = role;
+        this.active = active;
+        this.createdon = createdon;
+    }
+
+    public Users() {
+        
+    }
 }
