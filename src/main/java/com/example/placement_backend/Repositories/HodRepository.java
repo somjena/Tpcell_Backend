7880c873760bcb2fe501788b267d2497e8a2b1b7
Jyotiprajna Rout <131856771+jyotiprajna@users.noreply.github.com>
2023-11-18 23:13:22 +0530

    Create HodRepo
    extends from jpa

---
+++ src/main/java/com/example/placement_backend/Repositories/HodRepository.java
@@ -0,0 +1,7 @@
+package com.example.placement_backend.Repositories;
+
+import com.example.placement_backend.entity.HOD;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface HodRepository extends JpaRepository<HOD, Integer> {
+}
