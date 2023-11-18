9994422274f498c65cf24d68702b9cd483cb1977
Saktimayee Nayak <131857334+Sakti2345@users.noreply.github.com>
2023-11-18 10:10:38 +0530

    define All the fields of settings class
    commit

--- src/main/java/com/example/placement_backend/entity/Settings.java
+++ src/main/java/com/example/placement_backend/entity/Settings.java
@@ -1,4 +1,84 @@
 package com.example.placement_backend.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+@Entity
+
 public class Settings {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id ;
+    private String college;
+    private String address;
+    private String email;
+    private String phone;
+    private String website;
+    private String dean;
+
+    public Settings(String college, String address) {
+        this.college = college;
+        this.address = address;
+    }
+
+    public Settings() {
+        
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCollege() {
+        return college;
+    }
+
+    public void setCollege(String college) {
+        this.college = college;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getWebsite() {
+        return website;
+    }
+
+    public void setWebsite(String website) {
+        this.website = website;
+    }
+
+    public String getDean() {
+        return dean;
+    }
+
+    public void setDean(String dean) {
+        this.dean = dean;
+    }
 }
