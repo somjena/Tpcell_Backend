812660cd38d33d87f45fafaf543183d704fd8f50
Jyotiprajna Rout <131856771+jyotiprajna@users.noreply.github.com>
2023-11-19 22:37:33 +0530

    Created Hod Service
    commit

---
+++ src/main/java/com/example/placement_backend/services/HodService.java
@@ -0,0 +1,27 @@
+package com.example.placement_backend.services;
+
+import com.example.placement_backend.Repositories.HodRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class HodService {
+    @Autowired
+    HodRepository repo;
+
+    public int saveHOD(HOD hod) {
+        HOD hh=repo.save(hod);
+        return hh.getId();
+    }
+
+    public List<HOD> listAll(){
+        return repo.findAll(Sort.by(Direction.DESC, "id"));
+    }
+
+    public HOD getDetails(int id) {
+        return repo.getById(id);
+    }
+
+    public void deleteById(int id) {
+        HOD std=repo.getById(id);
+        repo.delete(std);
+    }
+}
---
+++ src/main/java/com/example/placement_backend/services/JobApplyService.java
@@ -0,0 +1,5 @@
+package com.example.placement_backend.services;
+
+public class JobApplyService {
+    
+}
