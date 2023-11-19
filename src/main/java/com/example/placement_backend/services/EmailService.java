3bf8e6d73926a522af466e302ec48ed5b76c5383
Saktimayee Nayak <131857334+Sakti2345@users.noreply.github.com>
2023-11-19 22:28:08 +0530

    upadte The Email Service
    commit

--- src/main/java/com/example/placement_backend/services/EmailService.java
+++ src/main/java/com/example/placement_backend/services/EmailService.java
@@ -1,4 +1,27 @@
 package com.example.placement_backend.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+
+import javax.mail.internet.MimeMessage;
+
 public class EmailService {
+
+    @Autowired
+    private JavaMailSender emailSender;
+
+    public void sendMail(String to, String subject, String body) {
+        MimeMessage message=emailSender.createMimeMessage();
+        try {
+            message.setSubject(subject);
+            MimeMessageHelper helper=new MimeMessageHelper(message,true);
+            helper.setFrom("noreply@foodwastage.com");
+            helper.setTo(to);
+            helper.setText(body,true);
+            emailSender.send(message);
+        }catch(Exception ex) {
+            System.out.println("Error "+ex.getMessage());
+        }
+    }
 }
