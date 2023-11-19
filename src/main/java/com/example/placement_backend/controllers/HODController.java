26fc693e780a70b14be63d6ebe681c00fd827ad6
Brijesh20000 <131857610+Brijesh20000@users.noreply.github.com>
2023-11-19 23:26:53 +0530

    Update HODController.java
    commit

--- src/main/java/com/example/placement_backend/controllers/HODController.java
+++ src/main/java/com/example/placement_backend/controllers/HODController.java
@@ -6,8 +6,44 @@ import org.springframework.web.bind.annotation.RestController;
 
 @CrossOrigin
 @RestController
-@RequestMapping("/api/company")
+@RequestMapping("/api/HOD")
 public class HODController {
 
+    @Autowired UsersService uservice;
+    @Autowired HodService hservice;
 
+    @PostMapping
+    public ResponseEntity<?> save(@RequestBody HodDto dto) {
+        int id=hservice.saveHOD(HodDto.toEntity(dto));
+        Users user=new Users(dto.getEmail(),dto.getName(),dto.getPwd(),id,"HOD",true);
+        uservice.AddUser(user);
+        return Response.success("HOD Created Successfully");
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<?> update(@PathVariable("id") int id, @RequestBody HodDto dto) {
+        HOD hod=HodDto.toEntity(dto);
+        hod.setId(id);
+        hservice.saveHOD(hod);
+        return Response.success("HoD updated successfully");
+    }
+
+    @GetMapping
+    public ResponseEntity<?> listAllHODs() {
+        List<HOD> result = hservice.listAll();
+        return Response.success(result);
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<?> findHODById(@PathVariable("id") int id) {
+        HOD result = hservice.getDetails(id);
+        return Response.success(result);
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<?> DeleteHODById(@PathVariable("id") int id) {
+        hservice.deleteById(id);
+        uservice.deleteUser(id);
+        return Response.success("HOD deleted successfully");
+    }
 }
