75dd8a5b22e2c746c0e285686efff91d0164830b
Brijesh20000 <131857610+Brijesh20000@users.noreply.github.com>
2023-11-19 23:28:52 +0530

    Update FileController.java
    update

--- src/main/java/com/example/placement_backend/controllers/FileController.java
+++ src/main/java/com/example/placement_backend/controllers/FileController.java
@@ -1,4 +1,20 @@
 package com.example.placement_backend.controllers;
 
 public class FileController {
+    @Autowired
+    private StorageService storageService;
+
+    @RequestMapping(value="/{fileName}", produces = "image/*")
+    public void download(@PathVariable("fileName") String fileName, HttpServletResponse resp) {
+        System.out.println("Loading file: " + fileName);
+        Resource resource = storageService.load(fileName);
+        if(resource != null) {
+            try(InputStream in = resource.getInputStream()) {
+                ServletOutputStream out = resp.getOutputStream();
+                FileCopyUtils.copy(in, out);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
