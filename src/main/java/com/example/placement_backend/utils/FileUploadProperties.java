ee65c2fba906097d8ad3e589f3a8bb2a3d578a4a
Soumitra jena <131749408+somjena@users.noreply.github.com>
2023-11-20 18:30:43 +0530

    Create FileUploadProperties.java
    commit

---
+++ src/main/java/com/example/placement_backend/utils/FileUploadProperties.java
@@ -0,0 +1,15 @@
+package com.example.placement_backend.utils;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "file.upload")
+public class FileUploadProperties {
+
+    private String location;
+    public String getLocation() {
+        return location;
+    }
+    public void setLocation(String location) {
+        this.location = location;
+    }
+}
